56e8aaefbc0d7efeb883df951c1c23939088bec7
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-12-17 23:03:04 +0300

    Create ListContacts.jsx

---
+++ src/components/ListContacts/ListContacts.jsx
@@ -0,0 +1,26 @@
+import { ElementListContacts } from 'components/ElementListContacts';
+import { List, ElementList } from './ListContactsStyled';
+import { useGetContactsQuery } from 'redux/contactsSlice';
+import { selectFilterValue } from 'redux/selectors';
+import { useSelector } from 'react-redux';
+
+export const ListContacts = () => {
+  const { data: contacts = [] } = useGetContactsQuery();
+
+  const filterValue = useSelector(selectFilterValue);
+  const normalyzeFilter = filterValue.toLowerCase();
+
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalyzeFilter)
+  );
+
+  return (
+    <List>
+      {visibleContacts.map(({ name, phone, id }) => (
+        <ElementList key={id}>
+          <ElementListContacts name={name} phone={phone} id={id} />
+        </ElementList>
+      ))}
+    </List>
+  );
+};
